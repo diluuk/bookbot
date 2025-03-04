fc58800911198593d5205dcce17f32db0740bf30
diluuk <diluuk@users.noreply.github.com>
2025-03-03 21:48:24 -0600

    added get_num_words function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     print("Starting main function...")
     text = get_book_text("books/frankenstein.txt")
-    print(text.strip())  # Full output
+    num_words = get_num_words(text.strip())
+    print(f"{num_words} words found in the document")
 
 def get_book_text(path):
     print(f"Attempting to open file: {path}")
@@ -9,6 +10,10 @@ def get_book_text(path):
         contents = f.read()
     print("File opened and read successfully.")
     return contents
+
+def get_num_words(contents):
+    words = contents.split()
+    return len(words)
 
 # Entry point
 if __name__ == "__main__":
