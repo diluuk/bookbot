e7ca837cee62c4dab24846cbfd4a46e3f9c02b16
diluuk <diluuk@users.noreply.github.com>
2025-03-03 22:38:33 -0600

    added get_num_char function

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_char
 
 def main():
     print("Starting main function...")
     text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(text.strip())
     print(f"{num_words} words found in the document")
+    num_chars = get_num_char(text.strip())
+    print("\nCharacter counts:")
+    for char, count in sorted(num_chars.items()):
+        print(f"'{char}': {count}")
 
 def get_book_text(path):
     print(f"Attempting to open file: {path}")
--- stats.py
+++ stats.py
@@ -1,3 +1,10 @@
 def get_num_words(contents):
     words = contents.split()
     return len(words)
+
+def get_num_char(contents):
+    full_text = contents.lower()
+    words_dict = {}
+    for char in full_text:
+        words_dict[char] = words_dict.get(char, 0) + 1
+    return words_dict
